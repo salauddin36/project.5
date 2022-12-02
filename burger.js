1f9dc545ec3d1a4db696bc917c8a9663b7f1a725
salauddin36 <salauddin36ayubi@gmail.com>
2022-12-02 21:34:34 +0530

    p5

---
+++ burger.js
@@ -0,0 +1,10 @@
+//      burger  nav
+
+burger = document.querySelector(".burger")
+navbar = document.querySelector(".navbar")
+navlist = document.querySelector(".navlist")
+
+burger.addEventListener("click", ()=>{
+    navbar.classList.toggle("h-nav");
+    navlist.classList.toggle("v-class");
+})
